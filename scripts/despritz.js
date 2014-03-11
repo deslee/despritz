b25f0396cd450095fcf6972d788e23cde31b1a7b
desmond <desmondclee@gmail.com>
2014-03-11 02:50:57 -0500

    refactored pivot algorithm out of main code

--- scripts/despritz.js
+++ scripts/despritz.js
@@ -1,4 +1,4 @@
-define(function() {
+define(['findpivot'], function(pivot) {
 	String.prototype.replaceAt=function(index, text) {
     	return this.substr(0, index) + text + this.substr(index+1);
 	}
@@ -6,44 +6,6 @@ define(function() {
 	var options = {
 		box: document.getElementById('box'),
 	}
-
-	var pivot = function(word) {
-		var bestLetter = 1;
-		var wordLength = word.length;
-
-		switch (wordLength) {
-		case 1:
-			bestLetter = 0; // first
-			break;
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-			bestLetter = 1; // second
-			break;
-		case 6:
-		case 7:
-		case 8:
-		case 9:
-			bestLetter = 2; // third
-			break;
-		case 10:
-		case 11:
-		case 12:
-		case 13:
-			bestLetter = 3; // fourth
-			break;
-		default:
-			bestLetter = 4; // fifth
-		};
-
-		// maybe: make correction in case of space
-		if (word.charAt(bestLetter) == ' ') {
-			bestLetter--;
-		}
-
-		return bestLetter;
-	};
 
 	var generate_letter_element = function(c, pivot) {
 		var letter = document.createElement('span');
